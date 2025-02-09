c27abeb542bd2d18b981d2a38c3cbc0a984ed7b6
= <=>
2025-02-09 15:26:49 +0400

    change default lang to english

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -3,10 +3,10 @@ import {createNavigation} from 'next-intl/navigation';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['ar', 'en'],
+  locales: ['en', 'ar'],
  
   // Used when no locale matches
-  defaultLocale: 'ar'
+  defaultLocale: 'en'
 });
 
  
