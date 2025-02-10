37dfbc8b591b689a923e052b0572ef42e03d0bf6
= <=>
2025-02-10 20:50:10 +0400

    adding tiktok icon

--- data/socialLinks.js
+++ data/socialLinks.js
@@ -2,7 +2,7 @@ export const socialMediaLinks = [
   { id: 1, href: "https://www.facebook.com/profile.php?id=61572265313246", iconClassName: "icon-facebook" },
   { id: 2, href: "https://x.com/sciencehub_uae", iconClassName: "icon-twitter" },
   { id: 3, href: "https://www.instagram.com/sciencehub.uae?fbclid=IwY2xjawIFhp9leHRuA2FlbQIxMAABHUMOzpcoHvhc6UmPJin3C3qip-I0FJ4pNO6K_fC-9M8__1K8HTpp1eyBqw_aem_fGPV_e-Eg3nigEAJTLfTXg", iconClassName: "icon-instagram" },
-  { id: 4, href: "https://www.tiktok.com/@sciencehub.uae?is_from_webapp=1&sender_device=pc", iconClassName: "icon-tiktok" },
+  { id: 4, href: "https://www.tiktok.com/@sciencehub.uae?is_from_webapp=1&sender_device=pc" },
 ];
 
 //   iconClassName are from font awesome
--- src/components/common/Socials.jsx
+++ src/components/common/Socials.jsx
@@ -1,12 +1,14 @@
-import Link from "next/link";
+
 
 import { socialMediaLinks } from "@/data/socialLinks";
 import React from "react";
+import Image from "next/image";
 
 export default function Socials({ componentsClass, textSize }) {
   return (
     <>
       {socialMediaLinks.map((link, index) => (
+        link.iconClassName ?
         <a 
           key={index}
           className={componentsClass ? componentsClass : "p-4"}
@@ -14,6 +16,21 @@ export default function Socials({ componentsClass, textSize }) {
         >
           <i className={`${link.iconClassName} ${textSize}`}></i>
         </a>
+        : 
+        <a 
+          key={index}
+          className={componentsClass ? componentsClass : "p-4"}
+          href={link.href}
+        >
+          <Image
+            width={15}
+            height={15}
+            className="pb-2"
+            src="/assets/img/general/tiktok.svg"
+            alt="image"
+          />
+        </a>
+        
       ))}
     </>
   );
