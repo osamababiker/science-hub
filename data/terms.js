28be7e7120d7d6db5db4b04450e9a23d43fd526b
= <=>
2025-02-12 17:21:24 +0400

    translate the terms page

---
+++ data/terms.js
@@ -0,0 +1,56 @@
+export const enTerms = [
+    {title: "Using our services", bio: `You must follow any policies made available to you within the
+    Services. Don't misuse our Services. For example, don't
+    interfere with our Services or try to access them using a method
+    other than the interface and the instructions that we provide.
+    You may use our Services only as permitted by law, including
+    applicable export and re-export control laws and regulations. We
+    may suspend or stop providing our Services to you if you do not
+    comply with our terms or policies or if we are investigating
+    suspected misconduct. Using our Services does not give you
+    ownership of any intellectual property rights in our Services or
+    the content you access. You may not use content from our
+    Services unless you obtain permission from its owner or are
+    otherwise permitted by law. These terms do not grant you the
+    right to use any branding or logos used in our Services. Don't
+    remove, obscure, or alter any legal notices displayed in or
+    along with our Services.`},
+    {title: "Your content in our services", bio: ` When you upload, submit, store, send or receive content to or
+    through our Services, you give Front (and those we work with)
+    a worldwide license to use, host, store, reproduce, modify,
+    create derivative works (such as those resulting from
+    translations, adaptations or other changes we make so that
+    your content works better with our Services), communicate,
+    publish, publicly perform, publicly display and distribute
+    such content. The rights you grant in this license are for the
+    limited purpose of operating, promoting, and improving our
+    Services, and to develop new ones. This license continues even
+    if you stop using our Services (for example, for a business
+    listing you have added to Front Maps). Some Services may offer
+    you ways to access and remove content that has been provided
+    to that Service. Also, in some of our Services, there are
+    terms or settings that narrow the scope of our use of the
+    content submitted in those Services.`},
+    {title: "Pay Attention", bio: ` Our Services are very diverse, so sometimes additional terms
+    or product requirements (including age requirements) may
+    apply. Additional terms will be available with the relevant
+    Services, and those additional terms become part of your
+    agreement with us if you use.`}
+]
+
+
+export const arTerms = [
+    {title: "استخدام خدماتنا", bio: `يجب عليك اتباع أي سياسات متاحة لك ضمن الخدمات. لا تسيء استخدام خدماتنا. على سبيل المثال، لا تتدخل في خدماتنا أو تحاول الوصول إليها باستخدام طريقة أخرى غير الواجهة والإرشادات التي نقدمها.
+    لا يجوز لك استخدام خدماتنا إلا وفقًا لما يسمح به القانون، بما في ذلك قوانين ولوائح الرقابة على التصدير وإعادة التصدير المعمول بها. يجوز لنا تعليق أو إيقاف تقديم خدماتنا إليك إذا لم تمتثل لشروطنا أو سياساتنا أو إذا كنا نحقق في سوء سلوك مشتبه به. لا يمنحك استخدام خدماتنا ملكية أي حقوق ملكية فكرية في خدماتنا أو المحتوى الذي تصل إليه. لا يجوز لك استخدام المحتوى من خدماتنا ما لم تحصل على إذن من مالكه أو يُسمح لك بذلك بموجب القانون. لا تمنحك هذه الشروط الحق في استخدام أي علامة تجارية أو شعارات مستخدمة في خدماتنا. لا تقم بإزالة أو إخفاء أو تغيير أي إشعارات قانونية معروضة في أو مع خدماتنا.`},
+    {title: "محتواك في خدماتنا", bio: ` عندما تقوم بتحميل أو إرسال أو تخزين أو إرسال أو استلام محتوى إلى أو عبر خدماتنا، فإنك تمنح Front (والجهات التي نعمل معها)
+    ترخيصًا عالميًا لاستخدام واستضافة وتخزين وإعادة إنتاج وتعديل و
+    إنشاء أعمال مشتقة (مثل تلك الناتجة عن
+    الترجمات أو التعديلات أو التغييرات الأخرى التي نجريها حتى يعمل
+    المحتوى الخاص بك بشكل أفضل مع خدماتنا) والتواصل و
+    النشر والأداء العلني والعرض العلني وتوزيع
+    مثل هذا المحتوى. الحقوق التي تمنحها في هذا الترخيص هي لغرض محدود يتمثل في تشغيل خدماتنا والترويج لها وتحسينها وتطوير خدمات جديدة. يستمر هذا الترخيص حتى
+    إذا توقفت عن استخدام خدماتنا (على سبيل المثال، لقائمة أعمال أضفتها إلى Front Maps). قد تقدم لك بعض الخدمات
+    طرقًا للوصول إلى المحتوى الذي تم توفيره لهذه الخدمة وإزالته. أيضًا، في بعض خدماتنا، توجد
+    شروط أو إعدادات تضيق نطاق استخدامنا للمحتوى المقدم في تلك الخدمات.`},
+    {title: "إنتبه", bio: ` خدماتنا متنوعة للغاية، لذا قد تنطبق أحيانًا شروط إضافية أو متطلبات للمنتج (بما في ذلك متطلبات العمر). ستكون الشروط الإضافية متاحة مع الخدمات ذات الصلة، وتصبح هذه الشروط الإضافية جزءًا من اتفاقيتك معنا إذا استخدمت.`}
+]
