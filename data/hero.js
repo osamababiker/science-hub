431b11d7785ea483c6e7d52618d47938ac063bd0
= <=>
2025-02-16 12:44:03 +0400

    hom page

--- data/hero.js
+++ data/hero.js
@@ -1,10 +1,10 @@
 export const slidesData = [
   {
     id: 1,
-    bgImage: "/assets/img/general/6.jpg", 
+    bgImage: "/assets/img/general/1.jpeg", 
   },
-  {
-    id: 2,
-    bgImage: "/assets/img/general/1.jpeg",
-  } 
+  // {
+  //   id: 2,
+  //   bgImage: "/assets/img/general/6.jpg",
+  // }
 ];
